7faf2658bb921b75477aeb029963da0e11f3635e
marcopeg <marco.pegoraro@gmail.com>
2023-01-03 13:34:48 +0100

    Fix build

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
-import { DndProvider } from "react-dnd";
-import { HTML5Backend } from "react-dnd-html5-backend";
 import { loadable } from "./loadable";
 
 // Lazy Loaded Routes
@@ -24,8 +22,6 @@ export const router = createBrowserRouter([
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <DndProvider backend={HTML5Backend}>
-      <RouterProvider router={router} />
-    </DndProvider>
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
